b0b2a96ee2543f8aa06d7667874c0dc1805bc925
Anthony Fernado Torres Detquizan <atorrede@everis.com>
2021-08-18 20:49:27 -0500

    feat guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ValidarGuard } from './core/guards/validar.guard';
 import { PostComponent } from './post/post.component';
 
 const routes: Routes = [
   {
     path: 'post/:index',
+    canActivate: [ValidarGuard],
     component: PostComponent
   }
 ];
---
+++ src/app/core/guards/validar.guard.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ValidarGuard } from './validar.guard';
+
+describe('ValidarGuard', () => {
+  let guard: ValidarGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    guard = TestBed.inject(ValidarGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
