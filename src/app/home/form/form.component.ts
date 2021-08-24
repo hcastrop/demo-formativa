52360d8f71f3c459e5b1c638b50661bde8205856
Anthony Fernado Torres Detquizan <atorrede@everis.com>
2021-08-23 20:40:39 -0500

    feat formArray ejemplo

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'demo-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+    selector: 'demo-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+    form: FormGroup;
+    personsStore: any[] = [];
+    get persons() {
+        return this.form.controls.persons as FormArray;
+    }
+    constructor(private fb: FormBuilder) {
+        this.form = this.fb.group({
+            persons: this.fb.array([this.addGroup()]),
+        });
+    }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    onClickSave() {
+      this.personsStore.push(... this.persons.value);
+      this.persons.clear();
+      this.persons.reset();
+      this.persons.push(this.addGroup());
+    }
 
+    onClickDeletePerson(position: number) {
+      this.personsStore = this.personsStore.filter((_: any, index: number) => position !== index);
+    }
+
+    onClickAddGroup() {
+        this.persons.push(this.addGroup());
+    }
+
+    onClickDelete(index: number) {
+      this.persons.removeAt(index)
+    }
+
+    getFormGroup(item: any) {
+        return item as FormGroup;
+    }
+
+    private addGroup() {
+        return this.fb.group({ name: [null, null], lastName: [null, null] });
+    }
 }
--- src/app/home/form/form.module.ts
+++ src/app/home/form/form.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { FormRoutingModule } from './form-routing.module';
 import { FormComponent } from './form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { FormComponent } from './form.component';
   ],
   imports: [
     CommonModule,
-    FormRoutingModule
+    FormRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class FormModule { }
