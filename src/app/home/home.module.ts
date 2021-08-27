6064066dc78b53856129bf40615391a62ec56bb7
Hugo Castro <hcastrop@gmail.com>
2021-08-27 11:00:54 -0500

    Hugo Castro
    Hugo Castro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { ClienteComponent } from './cliente/cliente.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ClienteComponent
   ],
   imports: [
     CommonModule,
